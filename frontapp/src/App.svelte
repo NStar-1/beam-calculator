a8705d66c889eeab7af69139f709c69b557513fb
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2022-10-13 22:11:42 +0300

    initial

--- frontapp/src/App.svelte
+++ frontapp/src/App.svelte
@@ -1,30 +1,86 @@
 <script>
-	export let name;
+	import Header from './Header/Header.svelte';
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.AppWrapper{
 	}
+	.InlinePage{
+		display: flex;
+		flex-direction: row;
+		margin: 60px auto;
+		column-gap: 10px;
+		height: auto;
+		width: 100%;
+		justify-content: center;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	.ContentPage{
+		padding: 10px;
+		border-radius: 5px;
+		width: 70%;
+		background: white;
+		height: 100vh;
+		box-shadow: 0 0 10px rgba(153, 153, 153, 0.28);
+	}
+	.ADV {
+		width: 10%;
+		height: fit-content;
+		box-shadow: none;
+	}
+	@media (max-width: 480px) {
+		.Inline{
+			margin: 0;
 		}
 	}
+	h1{
+		color: #ff8d48;
+
+	}
+	p{
+	}
+	.CalculatorWrapper{
+		height: 600px;
+		border: #ff8d48 1px solid;
+		margin: 20px;
+		display: flex;
+		flex-direction: row;
+		column-gap: 10px;
+		padding: 10px;
+		justify-content: space-between;
+	}
+	.OptionsWrapper{
+		padding: 10px;
+		height: content-box;
+		width: 30%;
+		border:  #ff8d48 1px solid;
+	}
+	.PictureWrapper{
+		width: 70%;
+		padding: 10px;
+		height: content-box;
+		border:  #ff8d48 1px solid;
+	}
 </style>
+
+<div class="AppWrapper">
+	<Header/>
+	<div class="InlinePage">
+		<div class="ContentPage ADV">advertising</div>
+		<div class="ContentPage">
+			<h1>Beam calculator(Title)</h1>
+			<p>Some description about calculator(Description)</p>
+			<div class="CalculatorWrapper">
+				<div class="OptionsWrapper">
+					inputs
+				</div>
+				<div class="PictureWrapper">
+					picture
+				</div>
+			</div>
+		</div>
+		<div class="ContentPage ADV">advertising</div>
+	</div>
+</div>
---
+++ frontapp/src/Header/Header.svelte
@@ -0,0 +1,62 @@
+<script>
+</script>
+
+<style>
+    .HeaderWrapper{
+        display: flex;
+        position: sticky;
+        top: 0;
+        width: 100%;
+        height: 60px;
+        background: #ff9859;
+        box-shadow: 10px 0 50px rgba(153, 153, 153, 0.54);
+    }
+    .ContentHeader{
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        margin: 0 15%;
+        flex-direction: row;
+    }
+    button{
+        display: flex;
+        height: 100%;
+        border: none;
+        align-items: center;
+        color: #ffffff;
+        background: transparent;
+        text-decoration:1px solid white underline;
+        cursor: pointer;
+    }
+    button:active, button:hover{
+        background: #ff8d48;
+    }
+    .AppName{
+        display: flex;
+        height: 100%;
+
+        align-self: center;
+        color: white;
+
+    }
+    @media (max-width: 480px) {
+        .AppName{
+            height: 60%;
+        }
+    }
+</style>
+
+
+<div class="HeaderWrapper">
+
+    <div class="ContentHeader">
+        <img rel='icon' src='/assets/logo.svg' alt='icon' class="AppName"/>
+        <div style="flex-direction: row; display: flex">
+            <button>switch lng wbh</button>
+            <button on:click={()=> console.log('a')}>
+                About us
+            </button>
+        </div>
+
+    </div>
+</div>
