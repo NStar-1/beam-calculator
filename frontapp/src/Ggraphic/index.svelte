e14f19649720d9cf06c1bd1f11b9084e54ab5687
Ruslan Panasiuk <ruslan.panasiuk@protonmail.com>
2022-11-19 01:08:38 +0500

    feat: (wip) add dimension lines

--- frontapp/src/Ggraphic/index.svelte
+++ frontapp/src/Ggraphic/index.svelte
@@ -13,7 +13,7 @@
 
     svg.append("svg:defs").append("svg:marker")
       .attr("id", "triangle")
-      .attr("refX", 3)
+      .attr("refX", 5)
       .attr("refY", 3)
       .attr("markerWidth", 12)
       .attr("markerHeight", 12)
@@ -64,6 +64,55 @@
       .style('font-size', "18px")
       .text('F = 10H')
 
+    const points = [{x: 55, y: 155}, {x: 400, y: 155}]
+
+    const x0 = points[0].x
+    const y0 = points[0].y
+    const x1 = points[1].x
+    const y1 = points[1].y
+    const dim = svg.append('g')
+      .attr(
+        'transform', 
+        `rotate(10, ${x0}, ${y0}) translate(${x0}, ${y0})`
+      )
+
+    const dx = x0 - points[1].x
+    const dy = y0 - points[1].y
+    const length = Math.sqrt((dx * dx) + (dy * dy))
+    const offset = 30
+
+    dim.append('line')
+      .attr('x1', 0)
+      .attr('y1', -offset)
+      .attr('x2', length)
+      .attr('y2', -offset)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+      .attr("marker-start", "url(#triangle)")
+      .attr("marker-end", "url(#triangle)")
+
+    dim.append('line')
+      .attr('x1', 0)
+      .attr('y1', -offset - 10)
+      .attr('x2', 0)
+      .attr('y2', -offset + 35)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+
+    dim.append('line')
+      .attr('x1', length)
+      .attr('y1', -offset - 10)
+      .attr('x2', length)
+      .attr('y2', -offset + 35)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+
+    dim.append('text')
+      .attr('x', length / 2)
+      .attr('y', -40)
+      .attr('fill', 'black')
+      .style('font-size', "18px")
+      .text('Text')
   });
 
   afterUpdate(function() {
