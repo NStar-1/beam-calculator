2773c0d0b4115c5d61567afad76fff59acfa822a
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2023-01-22 17:57:12 +0300

    Dictionary

---
+++ frontapp/src/dictionary.js
@@ -0,0 +1,12 @@
+export const dict = {
+    en: {
+        app: {
+            title: "Beam calculator"
+        }
+    },
+    ru: {
+        app: {
+            title: "Калькулятор палки-копалки"
+        }
+    }
+}
