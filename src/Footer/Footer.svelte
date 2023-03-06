26840ca48ba5017ab504e0325d5f034ad91a3432
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2023-03-06 03:56:37 +0300

    fix: Add untracked files

---
+++ src/Footer/Footer.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+
+<div class="footer-wrapper">
+
+
+</div>
+
+
+<style>
+    .footer-wrapper{
+        display: flex;
+        z-index: 0;
+        position: fixed;
+        bottom: 0;
+        background-color: #ff9859;
+        height: 5vh;
+        width: 100%;
+    }
+    @media(max-width: 480px){
+        .footer-wrapper{
+            position: static;
+        }
+    }
+</style>
---
+++ src/pages/About.svelte
@@ -0,0 +1,7 @@
+<script></script>
+
+
+
+<h1>About page</h1>
+
+<style></style>
