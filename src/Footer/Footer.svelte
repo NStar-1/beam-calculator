ac4e364640efa4758d542917d86705a73cbd284e
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2023-03-16 01:05:15 +0300

    feat: Add github link

--- src/Footer/Footer.svelte
+++ src/Footer/Footer.svelte
@@ -1,10 +1,13 @@
 <script>
-
+    import { navigate } from "svelte-navigator";
 </script>
 
 
 <div class="footer-wrapper">
-
+    <div class="github-style" on:click={()=>navigate('https://github.com/NStar-1/beam-calculator')}>
+        <img alt="github image"  src="assets/icons/github_logo.svg" class="logo"/>
+        <p>project on github</p>
+    </div>
 
 </div>
 
@@ -15,9 +18,18 @@
         z-index: 0;
         position: fixed;
         bottom: 0;
-        background-color: #ff9859;
+        background-color: #ff8d48;
         height: 5vh;
         width: 100%;
+        padding: 10px 15%;
+    }
+    .logo{
+        height: 100%;
+        
+    }
+    .github-style{
+        display: flex;
+        cursor: pointer;
     }
     @media(max-width: 480px){
         .footer-wrapper{
