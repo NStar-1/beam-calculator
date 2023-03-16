dafe7c108d6959cebca6582a8bff613473c79898
Ruslan Panasiuk <ruslan.panasiuk@protonmail.com>
2023-03-17 03:54:52 +0500

    build: configure SPA

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,7 +16,8 @@ const config = {
       assets: 'build',
       fallback: null,
       precompress: false,
-      strict: true
+      strict: true,
+      appDir: 'internal',
     }),
     paths: {
       base: "/beam-calculator",
