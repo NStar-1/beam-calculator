5ea24251cbcc25505038058a33f8a4cfbe5993e5
Ruslan Panasiuk <ruslan.panasiuk@protonmail.com>
2023-03-20 01:19:05 +0500

    feat: about placeholder

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,11 @@
 <script></script>
 
 <h1>About page</h1>
+<p>
+  Beam Calculator is an Open Source software for stress analysis, based one
+  <a href="https://frame3dd.sourceforge.net/">Frame3DD</a>
+</p>
+<h2>Credits</h2>
+<p>TODO</p>
 
 <style></style>
